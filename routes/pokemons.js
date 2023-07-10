822d809b4b5a25221247fb3b0be1eddf19d9b3ce
Eike Merx <eike.merx@sapiens.com>
2023-07-10 11:01:41 +0200

    delete old code

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -3,14 +3,14 @@ const pokemonRoute = express.Router();
 const pokemonsDB = require("../dbs/pokemonsDB");
 
 const pokemons = pokemonsDB.pokemons;
-// console.log(pokemons);
+//console.log(pokemons);
 pokemonRoute.get("/", (req, res) => {
   res.status(500).send(pokemons);
 });
 
 pokemonRoute.get("/:id", (req, res) => {
   const { id } = req.params;
-  const targetPokemon = pokemons.find((pokemon) => pokemon.id == id); //Need to change string number to number
+  const targetPokemon = pokemons.find((pokemon) => pokemon.id == id);
   console.log(targetPokemon);
   res.status(500).send(targetPokemon);
 });
