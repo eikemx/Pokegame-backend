95ee529205e6d21cfb79f27c55ec6edb0fc92f7f
Eike Merx <eike.merx@sapiens.com>
2025-02-12 08:59:49 +0100

    fix routes

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -3,16 +3,24 @@ const pokemonRoute = express.Router();
 const pokemonsDB = require("../dbs/pokemonsDB");
 
 const pokemons = pokemonsDB.pokemons;
-//console.log(pokemons);
+
 pokemonRoute.get("/", (req, res) => {
-  res.status(500).send(pokemons);
+  try {
+    res.status(200).json(pokemons);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
 pokemonRoute.get("/:id", (req, res) => {
   const { id } = req.params;
   const targetPokemon = pokemons.find((pokemon) => pokemon.id == id);
-  console.log(targetPokemon);
-  res.status(500).send(targetPokemon);
+  console.log("TARGET POKEMON BE >>>", targetPokemon);
+  if (targetPokemon) {
+    res.status(200).json(targetPokemon);
+  } else {
+    res.status(500).send({ error: "Pokemon not found" });
+  }
 });
 
 module.exports = pokemonRoute;
