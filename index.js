f40fd5a085dc58b4d9b990753497e54cd6a4abd2
Eike Merx <eike.merx@sapiens.com>
2025-02-12 10:45:38 +0100

    update cors

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express();
 var cors = require("cors");
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: ["poke-quartett.netlify.app", "http://localhost:3000"], // Your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    credentials: true, // Allow credentials (cookies, authorization headers)
+    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
 
@@ -14,12 +16,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const pokemonRoute = require("./routes/pokemons");
-const PORT = process.env.PORT || 3088;
-
 app.use("/pokemons", pokemonRoute);
 app.get("/", (req, res) => {
   res.send("Hello Poke-World!");
 });
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
+
+const PORT = process.env.PORT || 3088;
+app.set("port", port);
 
 app.listen(PORT, () => {
   console.log(`Your Pokemon is waiting on port ${PORT}`);
